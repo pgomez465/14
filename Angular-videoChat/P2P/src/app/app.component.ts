0d536e991e584fd7f0f87f6e3faa0e924e207760
Mahmoud Abotaleb <mabotaleb@outlook.com>
2020-01-08 10:33:15 +0200

    final

--- Angular-videoChat/P2P/src/app/app.component.ts
+++ Angular-videoChat/P2P/src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -6,28 +6,46 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent implements OnInit {
+
+  @ViewChild('myVideo') myVideo: any;
+
   title = 'P2P';
 
   targetPeer: any;
   peer: any;
 
   ngOnInit () {
-    this.peer = new SimplePeer ({
-      initiator: location.hash === '#init',
-      trickle: false,
 
+    let video = this.myVideo.nativeElement;
+    // Uncomment the next line for other browsers
+    //navigator.getUserMedia = (navigator.mediaDevices.getUserMedia || navigator.getUserMedia || navigator.webkitGetUserMedia || navigator.mozGetUserMedia || navigator.msGetUserMedia);
+    navigator.getUserMedia({video: true, audio: true}, (stream)=> {
 
-    }) 
+      this.peer = new SimplePeer ({
+        initiator: location.hash === '#init',
+        trickle: false,
+        stream: stream,
 
-    this.peer.on('signal', (data)=> {
-      console.log(JSON.stringify(data));
+      }) 
 
-      this.targetPeer = data; 
-    })
+      this.peer.on('signal', (data)=> {
+        console.log(JSON.stringify(data));
 
-    this.peer.on('data', (data)=> {
-      console.log('Recieved DATA: ' + data);
+        this.targetPeer = data; 
+      })
 
+      this.peer.on('data', (data)=> {
+        console.log('Recieved DATA: ' + data);
+
+      })
+
+      this.peer.on('stream', (stream)=> {
+        video.srcObject = stream;
+        video.play();
+      })
+
+    }, (err)=> {
+      console.log('Connection Error' + err);
     })
   }
 
